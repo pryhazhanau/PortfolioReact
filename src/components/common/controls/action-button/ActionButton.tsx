1476772ef55f68306b75d52bdabbb9f7fc4a354e
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-02 17:03:40 +0100

    Update action button component

--- src/components/common/controls/action-button/ActionButton.tsx
+++ src/components/common/controls/action-button/ActionButton.tsx
@@ -4,14 +4,17 @@ import { FC } from 'react';
 
 interface ButtonProps {
   label: string;
+  className?: string;
   onClick: () => void;
 }
 
-const ActionButton: FC<ButtonProps> = ({ label, onClick }) => {
+const ActionButton: FC<ButtonProps> = ({ label, className, onClick }) => {
   return (
+    <div className={className}>
     <button className="action-button body-text" onClick={onClick}>
       {label}
     </button>
+    </div>
   );
 };
 
