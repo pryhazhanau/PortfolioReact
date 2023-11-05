5c26f6936c2b44f06f553e59005d3bc5153c803c
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-05 21:02:52 +0100

    Update feedback section layout

--- src/components/home/feedback-section/FeedbackSection.tsx
+++ src/components/home/feedback-section/FeedbackSection.tsx
@@ -1,37 +1,53 @@
 import "./FeedbackSection.css";
 import "../HomePage.css";
-import FeedbackCard from "./feedback-card/FeedbackCard"
+import FeedbackCard from "./feedback-card/FeedbackCard";
 
 function FeedbackSection() {
   return (
     <>
       <div className="feedback-container">
         <div className="feedback-wrapper">
-            <div className="feedback-text-container">
+          <div className="feedback-text-container">
             <h1 className="headline section-title">Feedbacks</h1>
             <div className="feedback-body-container">
-                <p className="body-text-spacero">The feedback from people I worked with with pleasure.</p>
+              <p className="body-text-spacero">
+                The feedback from people I worked with with pleasure.
+              </p>
             </div>
-            </div>
-            <div className="feedback-cards-container">
-                {
-                    feedbacksList.map((item) => (
-                        <FeedbackCard body={item.body} description={item.description} img={item.img}/>
-                    ))
-                }
-            </div>
+          </div>
+          <div className="feedback-cards-container">
+            {feedbacksList.map((item) => (
+              <div key={`${item.id}`}>
+                <FeedbackCard
+                  body={item.body}
+                  description={item.description}
+                  img={item.img}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
   );
 }
 
-import Arseny from "../../../assets/feedback/arseny.jpeg"
-import Roma from "../../../assets/feedback/roma.jpg"
+import Arseny from "../../../assets/feedback/arseny.jpeg";
+import Roma from "../../../assets/feedback/roma.jpg";
 
 const feedbacksList = [
-    { id: 0, description: "Arseni Vasilevski —\n Project Manager @Andersen", body: "You make a customer's vision a reality. Pleasant soft-skills, self-driven, reliable, true senior and perspective Teach Lead. Satisfied customers. Supportive team-member. Creative thinker.", img: Arseny },
-    { id: 0, description: "Roman Bakenbard — Lead Software Engineer @Constanta", body: "Outstanding iOS developer - As great in problem-solver as a team play, and a dedicated project leader.", img: Roma }
-]
+  {
+    id: 0,
+    description: "Arseni Vasilevski —\n Project Manager @Andersen",
+    body: "You make a customer's vision a reality. Pleasant soft-skills, self-driven, reliable, true senior and perspective Teach Lead. Satisfied customers. Supportive team-member. Creative thinker.",
+    img: Arseny,
+  },
+  {
+    id: 1,
+    description: "Roman Bakenbard — Lead Software Engineer @Constanta",
+    body: "Outstanding iOS developer - As great in problem-solver as a team play, and a dedicated project leader.",
+    img: Roma,
+  },
+];
 
 export default FeedbackSection;
