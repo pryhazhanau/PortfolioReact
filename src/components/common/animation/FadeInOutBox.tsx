1e6008fec885752f70387b9c8290f07a116b53d3
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-05 21:01:17 +0100

    Fix fade animation

--- src/components/common/animation/FadeInOutBox.tsx
+++ src/components/common/animation/FadeInOutBox.tsx
@@ -4,22 +4,29 @@ import { FC, ReactNode } from "react";
 interface FadeProps {
   children?: ReactNode;
   visible: boolean;
+  className?: string;
 }
 
-export const FadeInOutBox: FC<FadeProps> = ({ children, visible }) => {
+export const FadeInOutBox: FC<FadeProps> = ({
+  children,
+  visible,
+  className,
+}) => {
   return (
-    <AnimatePresence mode="popLayout" presenceAffectsLayout={false}>
-        {visible ? 
-      <motion.div
-        key={"fadeInOut"}
-        exit={{ opacity: 0 }}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.3, ease: "ease" }}
-      >
-        {children}
-      </motion.div> : null}
-    </AnimatePresence>
+      <AnimatePresence mode="popLayout">
+        {visible ? (
+          <motion.div
+          className={className}
+            key={"fadeInOut"}
+            exit={{ opacity: 0 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+          >
+            {children}
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
   );
 };
 
