fe58c69c98f9da6f5ca8e59706d54858d36db1f7
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-05 21:00:42 +0100

    Add animations for text

---
+++ src/components/common/animation/AnimatedTextLongFade.tsx
@@ -0,0 +1,35 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { FC, ReactNode } from "react";
+
+interface AnimatedTextLongFadeProps {
+    children?: ReactNode;
+    animKey: string;
+    delay?: number
+  }
+  
+  export const AnimatedTextLongFade: FC<AnimatedTextLongFadeProps> = ({
+    children,
+    animKey,
+    delay
+  }) => {
+    return (
+        <AnimatePresence mode="wait">
+          <motion.div style={{originY: 0}}
+            key={animKey}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              type: "keyframe",
+              duration: 1,
+              ease: "easeIn",
+              delay: delay 
+            }}
+          >
+              {children}
+          </motion.div>
+        </AnimatePresence>
+    );
+  };
+  
+  export default AnimatedTextLongFade;
+  
