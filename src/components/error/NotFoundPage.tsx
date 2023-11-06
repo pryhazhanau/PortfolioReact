17de098a5e3b2a217e19a6cbe621ca3f5fddc05e
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-06 22:09:55 +0100

    Deployment

--- src/components/error/NotFoundPage.tsx
+++ src/components/error/NotFoundPage.tsx
@@ -7,7 +7,6 @@ export default function NotFoundPage() {
 
   return (
     <div id="not-found-page">
-      <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{"Error"}</i>
