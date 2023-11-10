e15f76556261cf96e4e5051ef97d5eea36ff6100
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-10 14:09:59 +0100

    Update navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 import "./Navbar.css";
 import "../../common/css/gradient.css";
 import cvFile from "../../assets/cv.pdf";
-import { useState } from "react";
+import { FC, useState, useEffect } from "react";
 import ActionButton from "../common/controls/action-button/ActionButton";
+import MenuButton from "./mobile-menu/MobileMenu";
 
-function Navbar() {
+interface NavbarProps {
+  menuVisibilityChanged: (visible: boolean) => void;
+}
+
+const Navbar: FC<NavbarProps> = ({ menuVisibilityChanged }) => {
   const [isOnTop, setOnTop] = useState(true);
 
   const handleScroll = () => {
@@ -16,8 +21,21 @@ function Navbar() {
   };
 
   const onClick = () => {
-    window.open(cvFile, "_blank")
+    window.open(cvFile, "_blank");
   };
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   window.addEventListener("scroll", handleScroll);
 
@@ -29,28 +47,36 @@ function Navbar() {
             PRY.
           </a>
         </div>
-
-        <div className="menu">
-              <div className="menu-item-container">
-                <a className="menu-item gradient-hover body-text" href="/">
-                  Home
-                </a>
-              </div>
-              <div className="menu-item-container">
-                <a className="menu-item gradient-hover body-text" href="/career">
-                  Career
-                </a>
-              </div>
-              <div className="menu-item-container">
-                <a className="menu-item gradient-hover body-text" href="/portfolio">
-                  Portfolio
-                </a>
-              </div>
-              <ActionButton className="cv-button" label="CV" onClick={onClick} />
-        </div>
+        {windowWidth > 900 ? (
+          <div className="menu">
+            <div className="menu-item-container">
+              <a className="menu-item gradient-hover body-text" href="/">
+                Home
+              </a>
+            </div>
+            <div className="menu-item-container">
+              <a className="menu-item gradient-hover body-text" href="/career">
+                Career
+              </a>
+            </div>
+            <div className="menu-item-container">
+              <a
+                className="menu-item gradient-hover body-text"
+                href="/portfolio"
+              >
+                Portfolio
+              </a>
+            </div>
+            <ActionButton className="cv-button" label="CV" onClick={onClick} />
+          </div>
+        ) : (
+          <MenuButton
+            menuVisibilityChanged={(visible) => menuVisibilityChanged(visible)}
+          />
+        )}
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
