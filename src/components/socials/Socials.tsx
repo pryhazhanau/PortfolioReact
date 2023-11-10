d34d273ed9a31e6a5ba0760b9db9844c29b98330
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-10 14:10:17 +0100

    Update socials with constant links

--- src/components/socials/Socials.tsx
+++ src/components/socials/Socials.tsx
@@ -5,28 +5,60 @@ import linkedInImg from "../../assets/icons/linkedin.svg";
 import telegramImg from "../../assets/icons/telegram.svg";
 import githubImg from "../../assets/icons/github.svg";
 import { ReactSVG } from "react-svg";
+import { useEffect, useState } from "react";
 
 function Socials() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <div className="follow-me-container">
-        <div className="body-text follow-me-label">Contact Me</div>
-        <div className="follow-me-vertical-line"></div>
-        <div className="social-icons">
-          <a href="https://www.instagram.com/prigozhanov/" target="_blank" className="social-icon-link">
-            <ReactSVG className="social-icon" src={instImg} />
-          </a>
-          <a href="https://www.linkedin.com/in/prigozhanov/" target="_blank" className="social-icon-link">
-          <ReactSVG className="social-icon" src={linkedInImg} />
-          </a>
-          <a href="https://t.me/prigozhanov" target="_blank" className="social-icon-link">
-            <ReactSVG className="social-icon" src={telegramImg} />
-          </a>
-          <a href="https://github.com/Prigozhanov" target="_blank" className="social-icon-link">
-          <ReactSVG className="social-icon" src={githubImg} />
-          </a>
+      {windowWidth > 900 && (
+        <div className="follow-me-container">
+          <div className="body-text follow-me-label">Contact Me</div>
+          <div className="follow-me-vertical-line"></div>
+          <div className="social-icons">
+            <a
+              href="https://www.instagram.com/prigozhanov/"
+              target="_blank"
+              className="social-icon-link"
+            >
+              <ReactSVG className="social-icon" src={instImg} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/prigozhanov/"
+              target="_blank"
+              className="social-icon-link"
+            >
+              <ReactSVG className="social-icon" src={linkedInImg} />
+            </a>
+            <a
+              href="https://t.me/prigozhanov"
+              target="_blank"
+              className="social-icon-link"
+            >
+              <ReactSVG className="social-icon" src={telegramImg} />
+            </a>
+            <a
+              href="https://github.com/Prigozhanov"
+              target="_blank"
+              className="social-icon-link"
+            >
+              <ReactSVG className="social-icon" src={githubImg} />
+            </a>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
