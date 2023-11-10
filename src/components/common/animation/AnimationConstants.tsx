5123043aa1a7304a57e74a6e37dd59c20178d3ec
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-10 23:35:11 +0100

    Add animation constants

---
+++ src/components/common/animation/AnimationConstants.tsx
@@ -0,0 +1,14 @@
+const AnimationConstants = {
+  duration: {
+    superSlow: 2,
+    slow: 1,
+    standard: 0.6,
+    quick: 0.3,
+    superQuick: 0.1,
+  },
+  cubic: {
+    easeEase: [0.11, 0.94, 0.14, 1.01],
+  },
+};
+
+export default AnimationConstants;
