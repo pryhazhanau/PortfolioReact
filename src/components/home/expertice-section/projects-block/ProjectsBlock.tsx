10b668ab683fd62b7fa4b1d1fd712c5dacbce9da
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-13 20:55:50 +0100

    Implement projects block

---
+++ src/components/home/expertice-section/projects-block/ProjectsBlock.tsx
@@ -0,0 +1,27 @@
+import ActionButton from "../../../common/controls/action-button/ActionButton";
+import "./ProjectsBlock.css";
+
+import AppsImg from "../../../../assets/illustrations/apps_fade.png";
+
+function ProjectsBlock() {
+  return (
+    <>
+      <div className="projects-block">
+        <div className="projects-text-block">
+          <p className="body-text-spacero">
+            There are several projects I worked on. From large enerprise to
+            user-centric product applications.
+          </p>
+        </div>
+        <div className="projects-illistration-block">
+          <img src={AppsImg}></img>
+          <div className="projects-button-wrapper">
+            <ActionButton label="Explore" style="outline" link="/portfolio" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProjectsBlock;
