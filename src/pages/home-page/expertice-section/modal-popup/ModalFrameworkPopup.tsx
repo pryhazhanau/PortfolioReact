85971c163ba02f056227ef0cadc19b185dfa5042
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-15 20:02:21 +0100

    Implement modal popup

---
+++ src/pages/home-page/expertice-section/modal-popup/ModalFrameworkPopup.tsx
@@ -0,0 +1,12 @@
+
+function ModalFrameworkPopup() {
+    return (
+        <div className="modal-background">
+            <div className="modal-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default ModalFrameworkPopup
