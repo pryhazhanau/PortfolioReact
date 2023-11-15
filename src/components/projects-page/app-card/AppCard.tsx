e9c564730aecb35d1df31130ab6ec22c89c779a9
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-15 12:17:12 +0100

    Implement AppCard

---
+++ src/components/projects-page/app-card/AppCard.tsx
@@ -0,0 +1,33 @@
+import "./AppCard.css";
+import ActionButton from "../../common/controls/action-button/ActionButton";
+import { FC } from "react";
+
+interface AppCardProps {
+  app: AppProject
+}
+
+const AppCard: FC<AppCardProps> = ({app}) => {
+  return (
+    <div className="app-card">
+      <div>
+        <div className="card-icon-app-wrapper">
+          <img src={app.img}></img>
+          <p className="subtitle-primary-aluminor">{app.title}</p>
+        </div>
+        <div className="card-body-wrapper">
+          <p className="body-text-spacero">
+            {app.body}
+          </p>
+        </div>
+      </div>
+      
+      <div className="card-button-wrapper">
+        { app.link != undefined && (
+        <ActionButton label="explore" link={app.link} style="outline" linkTarget="blank"/>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default AppCard;
