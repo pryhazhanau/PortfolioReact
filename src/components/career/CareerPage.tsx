9194388a88e908b25dcc9fd10a5c11bfc2cc8208
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-15 12:19:10 +0100

    Update career page

--- src/components/career/CareerPage.tsx
+++ src/components/career/CareerPage.tsx
@@ -3,6 +3,7 @@ import "../../common/css/div-layout.css";
 import MasterContainer from "../master-container/MasterContainer";
 import Footer from "../footer/Footer";
 import CareerPathSection from "./path-section/CareerPathSection"
+import GradientSectionBackground from "../common/background/GradientSectionBackground"
 
 function CareerPage() {
   return (
@@ -11,6 +12,7 @@ function CareerPage() {
         <p className="headline-aluminor page-title">Career</p>
         <CareerPathSection/>
     </MasterContainer>
+    <GradientSectionBackground/>
     <Footer/>
       </>
   );
