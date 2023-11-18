46391dc23b2a9d9a11105b9358dcf8e07a01747d
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-18 17:09:48 +0100

    Fix texts

--- src/pages/home-page/about-section/AboutSection.tsx
+++ src/pages/home-page/about-section/AboutSection.tsx
@@ -11,11 +11,11 @@ function AboutSection() {
         <div className="about-content-container">
           <div className="about-flex-container">
             <h1 className="body-text-spacero">
-              My name is Uladzimir, and I am an iOS Developer hailing from
-              Warsaw, Poland. Although I was born in Minsk, Belarus, I've found
+              My name is Uladzimir, and I am an iOS Developer from
+              Warsaw, Poland. I was born in Minsk, Belarus. I've found
               my professional calling in the world of iOS and MacOS Development.
               With a solid and comprehensive foundation in this field, I've
-              spent the last four years refining my iOS development skills
+              spent almost five years refining my iOS development skills
               through a combination of professional experiences and personal
               projects. I possess an in-depth understanding of Swift and
               Objective-C, along with a profound familiarity with the iOS SDK.
