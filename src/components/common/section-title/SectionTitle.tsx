e1e85882ebba5ad72b3e91b4cb7545e6f449d185
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-11-21 16:49:58 +0100

    Update section title

--- src/components/common/section-title/SectionTitle.tsx
+++ src/components/common/section-title/SectionTitle.tsx
@@ -1,25 +1,38 @@
-import "./SectionTitle.css"
+import "./SectionTitle.css";
 import { FC } from "react";
-import Text from "../../common/style/Text"
+import Text from "../../common/style/Text";
 import { Typography } from "../style/interface/Typography";
 import { Colors } from "../style/interface/Colors";
 import FlexBox from "../box/FlexBox";
 
 interface SectionTitleProps {
-    sectionName: string;
-    sectionDesc: string;
+  sectionName: string;
+  sectionDesc: string;
 }
 
-const SectionTitle: FC<SectionTitleProps> = ({sectionName, sectionDesc}) => {
-    return (
-        <FlexBox direction="column" justifyContent="start" alignItems="start" margin={{bottom: 30, top: 30}}>
-            <div className="section-pill">
-                <p className="section-pill-text description">{sectionName}</p>
-            </div>
-            <Text text={sectionDesc} typography={Typography.SubtitlePrimary} color={Colors.AluminorGray} margin={{top: 20, bottom: 6}} maxWidth={500}/>
-            <div className="section-title-devider "></div>
-        </FlexBox>
-    )
-}
+const SectionTitle: FC<SectionTitleProps> = ({ sectionName, sectionDesc }) => {
+  return (
+    <FlexBox
+      direction="column"
+      justifyContent="start"
+      alignItems="start"
+      margin={{ bottom: 30, top: 30 }}
+    >
+      <FlexBox gap={12}>
+        <div className="section-pill">
+          <p className="section-pill-text description">{sectionName.toUpperCase()}</p>
+        </div>
+      </FlexBox>
+      <Text
+        text={sectionDesc}
+        typography={Typography.Title}
+        color={Colors.AluminorGray}
+        margin={{ top: 20, bottom: 6 }}
+        maxWidth={500}
+      />
+      <div className="section-title-devider "></div>
+    </FlexBox>
+  );
+};
 
-export default SectionTitle
+export default SectionTitle;
