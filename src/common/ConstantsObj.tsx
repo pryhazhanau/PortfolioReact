016d4d9e8277477ac18140e5bf136eb638d89350
Vladimir Prigozhanov <vprigozhanov@gmail.com>
2023-12-02 16:11:05 +0100

    Update social links

--- src/common/ConstantsObj.tsx
+++ src/common/ConstantsObj.tsx
@@ -2,10 +2,10 @@ const Constants = {
   contact: {
     address: { street: "Grodkowska", index: "01-461", country: "Poland", city: "Warsaw", link: "https://www.google.com/maps/place/Grodkowska,+01-460+Warszawa/@52.2416651,20.9214039,17z/data=!3m1!4b1!4m6!3m5!1s0x471ecb1b3959e5a1:0x3833a0ada91d8caf!8m2!3d52.2416651!4d20.9239788!16s%2Fg%2F1vysv8x5?entry=ttu"},
     email: { display: "vprigozhanov@gmail.com", link: "mailto:vprigozhanov@gmail.com?subject=Hello, Uladzimir" },
-    linkedIn: { display: "in/prigozhanov", link: "https://www.linkedin.com/in/prigozhanov" },
+    linkedIn: { display: "in/pryhazhanau", link: "https://www.linkedin.com/in/pryhazhanau" },
     telegram: { display: "@prigozhanov", link: "https://t.me/prigozhanov" },
     github: { display: "GitHub", link: "https://github.com/Prigozhanov" },
-    instagram: { display: "Instagram", link: "https://www.instagram.com/prigozhanov" }
+    instagram: { display: "Instagram", link: "https://www.instagram.com/pryhazhanau.dev" }
   },
 };
 
